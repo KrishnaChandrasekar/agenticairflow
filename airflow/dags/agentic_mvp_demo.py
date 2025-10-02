4ff0c6391bdb01462fea8f2dea389c5d979f3f5a
KRISHNA KUMAR C <kicha@KRISHNAs-MacBook-Pro.local>
2025-10-03 00:09:36 +0530

    rename operator and dag usages

--- airflow/dags/agentic_mvp_demo.py
+++ airflow/dags/agentic_mvp_demo.py
@@ -1,7 +1,7 @@
 
 from datetime import datetime
 from airflow import DAG
-from agentic_ssh.AgenticSSH_operators import AgenticSSHOperator
+from agentic_ssh.AgenticSSH_operators import AgenticRunOperator
 
 with DAG(
     dag_id="agentic_mvp_demo",
@@ -10,19 +10,19 @@ with DAG(
     catchup=False,
     tags=["agentic-demo"],
 ):
-    vm1_ok = AgenticSSHOperator(
+    vm1_ok = AgenticRunOperator(
         task_id="vm1_ok",
         agent_id="vm1",
         command="echo 'vm1 starting'; for i in 1 2 3 4 5; do echo tick:$i; sleep 2; done; echo 'vm1 done'",
         cwd="/",
     )
-    go_vm1_ok = AgenticSSHOperator(
+    go_vm1_ok = AgenticRunOperator(
         task_id="go_vm1_ok",
         agent_id="go_vm1",
         command="echo 'go_vm1 starting'; for i in 1 2 3 4 5; do echo tick:$i; sleep 10; done; echo 'go_vm1 done'",
         cwd="/",
     )
-    vm4_ok = AgenticSSHOperator(
+    vm4_ok = AgenticRunOperator(
         task_id="vm4_ok",
         agent_id="vm4",
         command="echo 'this should run at the end'",
